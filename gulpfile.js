896564644d81ef444a05ed84549bf9eeea0b3b63
iFrosta <samgolovchenko@gmail.com>
2019-10-09 01:07:56 +0300

    template gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,94 @@
-var gulp = require('gulp'); // require gulp
-var sass = require('gulp-sass');
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
+(() => {
 
-// CSS section
-gulp.task('sass', function(){
-    return gulp.src('app/public/scss/layout.scss')
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
-        .pipe(gulp.dest('app/public/css'))
-});
-gulp.task('autoprefixer', () => {
-    const autoprefixer = require('autoprefixer')
-    const sourcemaps = require('gulp-sourcemaps')
-    const postcss = require('gulp-postcss')
+    'use strict';
 
-    return gulp.src('./src/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer() ]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dest'))
-});
-gulp.task('default', function () {
-    gulp.src('src/**/*.css')
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist'));
-});
+    /**************** gulpfile.js configuration ****************/
+
+    const
+
+        // development or production
+        devBuild  = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development'),
+
+        // directory locations
+        dir = {
+            src         : 'src/',
+            build       : 'build/'
+        },
+
+        // modules
+        gulp          = require('gulp'),
+        noop          = require('gulp-noop'),
+        newer         = require('gulp-newer'),
+        size          = require('gulp-size'),
+        imagemin      = require('gulp-imagemin'),
+        sass          = require('gulp-sass'),
+        postcss       = require('gulp-postcss'),
+        sourcemaps    = devBuild ? require('gulp-sourcemaps') : null,
+        browsersync   = devBuild ? require('browser-sync').create() : null;
+
+    /**************** CSS task ****************/
+    const cssConfig = {
+
+        src         : dir.src + 'scss/main.scss',
+        watch       : dir.src + 'scss/**/*',
+        build       : dir.build + 'css/',
+        sassOpts: {
+            sourceMap       : devBuild,
+            imagePath       : '/images/',
+            precision       : 3,
+            errLogToConsole : true
+        },
+
+        postCSS: [
+            require('usedcss')({
+                html: ['index.html']
+            }),
+            require('postcss-assets')({
+                loadPaths: ['images/'],
+                basePath: dir.build
+            }),
+            require('autoprefixer')({
+                browsers: ['> 1%']
+            }),
+            require('cssnano')
+        ]
+
+    };
+
+    function css() {
+
+        return gulp.src(cssConfig.src)
+            .pipe(sourcemaps ? sourcemaps.init() : noop())
+            .pipe(sass(cssConfig.sassOpts).on('error', sass.logError))
+            .pipe(postcss(cssConfig.postCSS))
+            .pipe(sourcemaps ? sourcemaps.write() : noop())
+            .pipe(size({ showFiles: true }))
+            .pipe(gulp.dest(cssConfig.build))
+            .pipe(browsersync ? browsersync.reload({ stream: true }) : noop());
+
+    }
+    exports.css = gulp.series(images, css);
+
+    /**************** images task ****************/
+    const imgConfig = {
+        src           : dir.src + 'img/**/*',
+        build         : dir.build + 'img/',
+        minOpts: {
+            optimizationLevel: 5
+        }
+    };
+
+    function images() {
+
+        return gulp.src(imgConfig.src)
+            .pipe(newer(imgConfig.build))
+            .pipe(imagemin(imgConfig.minOpts))
+            .pipe(size({ showFiles:true }))
+            .pipe(gulp.dest(imgConfig.build));
+
+    }
+    exports.images = images;
+
+    console.log('Gulp', devBuild ? 'development' : 'production', 'build');
+
+})();
